a444203fc59739ba3888c17a8980bc1fa6544d13
JimTheChickenty <paulavorih9@gmail.com>
2026-06-23 16:05:47 +0200

    The app/database will now store usernames, not just emails

--- models/User.js
+++ models/User.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+username: {
+  type: String,
+  unique: true,
+  sparse: true,
+  trim: true,
+  minlength: 3
+},
+
   email: {
     type: String,
     required: true,
